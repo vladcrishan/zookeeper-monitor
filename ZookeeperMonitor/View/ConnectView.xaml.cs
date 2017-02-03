4fc21f9c05374077021236a637520826f54115c1
Vlad Crișan <vlad.crishan20@gmail.com>
2017-02-03 17:05:36 +0200

    first commit

---
+++ ZookeeperMonitor/View/ConnectView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using ZookeeperMonitor.ViewModel;
+
+namespace ZookeeperMonitor.View
+{
+    /// <summary>
+    /// Interaction logic for ConnectView.xaml
+    /// </summary>
+    public partial class ConnectView : Window
+    {
+        public ConnectView()
+        {
+            InitializeComponent();
+            var connectViewModel = new ConnectViewModel();
+
+            DataContext = connectViewModel;
+            connectViewModel.ButtonClicked += this.Hide;
+        }
+    }
+}
---
+++ ZookeeperMonitor/ViewModel/ConnectViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Input;
+using ZookeeperMonitor.Command;
+using ZookeeperMonitor.Model;
+using ZookeeperMonitor.View;
+
+namespace ZookeeperMonitor.ViewModel
+{
+    class ConnectViewModel
+    {
+        private MainModel _mainModel;
+        private ZKViewModel _zkViewModel;
+
+        public delegate void ButtonClickEvent();
+        public event ButtonClickEvent ButtonClicked = delegate { }; 
+
+        public ConnectViewModel()
+        {
+            _mainModel = new MainModel();
+
+            //HostName = "10.10.10.99"; // local zookeeper
+            //HostName = "cargo301,cargo302,cargo303";
+            ConnectCommand = new RelayCommand(ConnectAction);
+        }
+
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// Commands
+        /// </summary>
+        public ICommand ConnectCommand { get; private set; }
+
+        /// <summary>
+        /// Actions
+        /// </summary>
+        public void ConnectAction()
+        {
+            // Event
+            ButtonClicked();
+
+            //Connect
+            _mainModel.Connect(HostName);
+
+            // View + ViewModel of ZooKeeper main window.
+            _zkViewModel = new ZKViewModel(_mainModel);
+            var zkView = new ZooKeeperView()
+            {
+                DataContext = _zkViewModel
+            };
+            zkView.ShowDialog();
+        }
+
+    }
+}
